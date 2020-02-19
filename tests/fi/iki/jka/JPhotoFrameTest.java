7fa9b213b4946e2eba807e69bec7ebeb5391652a
dmitrykos <dmitrykos@neutroncode.com>
2020-02-19 12:19:14 +0000

    updated tests

--- tests/fi/iki/jka/JPhotoFrameTest.java
+++ tests/fi/iki/jka/JPhotoFrameTest.java
@@ -2,12 +2,56 @@ package fi.iki.jka;
 
 import org.junit.Test;
 
+import java.awt.event.ActionEvent;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
 public class JPhotoFrameTest {
+
+    @Test(expected = NullPointerException.class)
+    public void testStartSlideShowIfPhotosAndListAreNull() {
+        JPhotoShow show = new JPhotoShow(null, 5000, null);
+    }
+
     @Test
-    public void placeholder() throws Exception {
-        assertThat(2, equalTo(2));
+    public void testGetSlideShowInterval() throws Exception {
+
+        String frameName = "test1";
+        JPhotoCollection photos = new JPhotoCollection();
+
+        JPhotoFrame frame = new JPhotoFrame()
+        {
+            @Override
+            public void setTitle()
+            { }
+
+            @Override
+            void handleStartSlideshow(int interval)
+            {
+                assertThat(interval, equalTo(JPhotoFrame.DEFAULT_SLIDE_SHOW_INTERVAL));
+            }
+        };
+
+        frame.actionPerformed(new ActionEvent(this, 0, JPhotoMenu.A_SLIDESHOW));
+    }
+
+    @Test
+    public void testSetSlideShowInterval() throws Exception {
+
+        JPhotoFrame frame = new JPhotoFrame()
+        {
+            @Override
+            public void setTitle()
+            { }
+
+            @Override
+            void handleStartSlideshow(int interval)
+            {
+                assertThat(interval, equalTo(JPhotoFrame.DEFAULT_SLIDE_SHOW_INTERVAL / 2));
+            }
+        };
+
+        frame.actionPerformed(new ActionEvent(this, 0, JPhotoMenu.A_SLIDESHOW_2XF));
     }
 }
